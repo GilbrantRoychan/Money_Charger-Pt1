49e5eaa0b2936458acae9c525985568150a5eae7
GilbrantRoychan <gilbrant.roychan123@gmail.com>
2025-09-14 18:12:11 +0700

    first commit and first configuration

---
+++ src/main/java/roychan/gill/money/currency/App.java
@@ -0,0 +1,13 @@
+package roychan.gill.money.currency;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/roychan/gill/money/currency/AppTest.java
@@ -0,0 +1,20 @@
+package roychan.gill.money.currency;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
